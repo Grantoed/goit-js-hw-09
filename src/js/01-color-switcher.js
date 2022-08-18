b8cfb8811f351d7d39b49d0dfb1bc6bbdd7f5e67
Ruslan Babaiev <ruslan.nadirovich@gmail.com>
2022-08-19 00:15:50 +0300

    task 1 solved

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const INTERVAL_TIME = 1000;
+let intervalId = null;
 
+const refs = {
+  body: document.querySelector('body'),
+  start: document.querySelector('[data-start]'),
+  stop: document.querySelector('[data-stop]'),
+};
+
+refs.start.addEventListener('click', onStart);
+refs.stop.addEventListener('click', onStop);
+
+function onStart() {
+  refs.start.disabled = true;
+  refs.stop.disabled = false;
+
+  intervalId = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, INTERVAL_TIME);
+}
+
+function onStop() {
+  refs.stop.disabled = true;
+  refs.start.disabled = false;
+
+  clearInterval(intervalId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
